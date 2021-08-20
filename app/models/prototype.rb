bab4afbf48f1d89cf2299226dd7363ab7689d662
chachamaru6 <kotaro.tech0130@gmail.com>
2021-08-20 18:59:30 +0900

    second　commit

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -7,5 +7,5 @@ class Prototype < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  has_many :comments   # commentsテーブルとのアソシエーション
+  has_many :comments, dependent: :destroy  # commentsテーブルとのアソシエーション
 end
